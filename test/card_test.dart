12453dbd64df5941d62bd96fd5b1c95921c7d7a0
Leo Honkanen <leo.o.honkanen@gmail.com>
2015-09-12 18:53:51 +0300

    add support for basic features

--- test/card_test.dart
+++ test/card_test.dart
@@ -11,20 +11,40 @@ void main() {
   group('Card', () {
     /// load some cards
     var some_cards;
+    var collection;
 
     setUp(() {
       var cardres = new Resource("package:brawl/res/some_cards.json");
       var cardfuture = cardres.readAsString();
+      collection = new Collection();
       return cardfuture.then((carddata) =>
           some_cards = new JsonDecoder().convert(carddata));
     });
 
-    var collection = new Collection();
-
+    /// initial sanity checks
     group('parse', () {
-      test('should parse a simple card', () {
+      test('accepts simple card', () {
         var card = collection.parseCard(some_cards[0]);
         expect(card, isNotNull);
+      });
+      test('puts card on collection', () {
+        var card = collection.parseCard(some_cards[0]);
+        expect(collection.cards.length, equals(1));
+      });
+      test('puts some card on collection on key', () {
+        var card = collection.parseCard(some_cards[0]);
+        expect(collection.cards[card.name], isNotNull);
+      });
+      test('puts the card on collection on key', () {
+        var card = collection.parseCard(some_cards[0]);
+        expect(collection.cards[card.name], equals(card));
+      });
+    });
+
+    group("feature", () {
+      test('works for taunt', () {
+        var card = collection.parseCard(some_cards[1]);
+        expect(card.features.length, equals(1));
       });
     });
   });
